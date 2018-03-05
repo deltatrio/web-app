1b7e65c67f8fe620744e0faef11e15163bc7b7f7
kaylaobritsch <obritschk@gmail.com>
2018-03-05 13:29:08 -0500

    Fix JS for the tabs

--- js/main.js
+++ js/main.js
@@ -31,3 +31,21 @@ function delay (URL) {
     window.location = URL
   }, 2500 );
 }
+
+document.getElementById("defaultOpen").click();
+function openRated(evt, ratedName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(ratedName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
